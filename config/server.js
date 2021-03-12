de40feaeeb26bf5c75e47bce861e59b166d6102a
Ubuntu <ubuntu@ip-172-31-11-209.ca-central-1.compute.internal>
2021-03-12 06:24:52 +0000

    random commit

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 80),
+  port: env.int('PORT', 8080),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '9915bc803390bbf78fbe186cc83204a3'),
